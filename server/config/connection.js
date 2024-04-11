922a607722b9a98a9dadb31383f3b652812fc767
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-10 23:37:20 -0700

    ongoing Render deployment issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@
 
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error(err));
