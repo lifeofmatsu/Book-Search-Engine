55fa4e03caac7cd6d7e496a7ca54c0ffe2272a39
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-10 20:17:11 -0700

    minor revisions

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,4 +6,20 @@ mongoose.connect(
     "mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   );
 
+  const connectDB = async () => {
+    try {
+      await mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('MongoDB Connected...');
+    } catch (err) {
+      console.error(err.message);
+      // Exit process with failure
+      process.exit(1);
+    }
+  };
+  
+  module.exports = connectDB;
+
 module.exports = mongoose.connection;
