2e21b68337b49d8a9f79b873683f7a9b8e0cba51
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-10 22:01:24 -0700

    ongoing db connection issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0');
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0');
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error(err));
 
-module.exports = mongoose.connection;
+
+  module.exports = mongoose.connection;
