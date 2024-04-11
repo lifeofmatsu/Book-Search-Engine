f57b29d19aabd27e061f25e6f689adc99f81ac5f
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-10 20:58:55 -0700

    minor revisions

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+mongoose.connect('mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0');
 
 // mongoose.connect(
 //     "mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
