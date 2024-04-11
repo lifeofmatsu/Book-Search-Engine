7282c572a9565c8f7b937ab2b2c459af04ab29f3
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-10 20:18:16 -0700

    minor revisions

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 mongoose.connect(
-    "mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://lifeofmatsu:Live2swim*0826@cluster0.wbu9lfp.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0"
   );
 
-  const connectDB = async () => {
-    try {
-      await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('MongoDB Connected...');
-    } catch (err) {
-      console.error(err.message);
-      // Exit process with failure
-      process.exit(1);
-    }
-  };
+//   const connectDB = async () => {
+//     try {
+//       await mongoose.connect(mongoURI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+//       console.log('MongoDB Connected...');
+//     } catch (err) {
+//       console.error(err.message);
+//       // Exit process with failure
+//       process.exit(1);
+//     }
+//   };
   
-  module.exports = connectDB;
+//   module.exports = connectDB;
 
 module.exports = mongoose.connection;
